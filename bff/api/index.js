88072b4e2b66e90c0c98ab4328a25b98deef26e2
fernando-nunes21 <luis@ubots.com.br>
2022-01-11 08:12:42 -0300

    Bff update start

--- bff/api/index.js
+++ bff/api/index.js
@@ -1,14 +1,17 @@
+var http = require('http');
 const express = require('express');
 const path = require('path')
 const app = express();
 const port = 3001;
 
+const server = http.createServer(app);
+
 app.use(express.json());
 app.use("/api/heroes", require('./routes/heroes.routes'))
+app.use('/static',express.static(__dirname+"../../../client/build/static"))
+app.use('/',express.static(__dirname+"../../../client/build"))
 app.get('/', (req, res) => {
-    res.sendFile(path.join('client/public' + '/index.html'))
-  })//Deixar disponivel os arquivos client
+  res.sendFile(path.resolve(__dirname+'../../../client/build/index.html'));
+  })
 
-app.listen(port, () => {
-    console.log(`Bff is running on port ${port}`);
-});
+server.listen(port);
